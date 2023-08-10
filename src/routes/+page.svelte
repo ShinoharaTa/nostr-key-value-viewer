792b85c2aa7cf85a6f2271bfeeb25ef6bb7adc59
tshinohara <tshinohara@spiritek.co.jp>
2023-08-10 12:46:26 +0900

    バグ修正

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,9 @@
   // "nostr-arrival-rate_kirino";
   let authorHex = $page.url.searchParams.get("hex") ?? "";
   // "a3c13ef4c9eccfde01bd9326a2ab08b2ad7dc57f3b77db77723f8e2ad7ba24d6";
-  let relays = ($page.url.searchParams.get("relays") ?? "wss://relay-jp.nostr.wirednet.jp").replace(",", "\n")
+  let relays = (
+    $page.url.searchParams.get("relays") ?? "wss://relay-jp.nostr.wirednet.jp"
+  ).replace(",", "\n");
   let result = "";
   const getDatabase = async () => {
     result = "processing...";
@@ -32,21 +34,21 @@
 <div class="max-width mx-auto py-4">
   <div class="mb-3">
     <label for="" class="form-label">table name:</label>
-    <input type="text" class="form-control" value={tableName} />
+    <input type="text" class="form-control" bind:value={tableName} />
   </div>
   <div class="mb-3">
     <label for="" class="form-label">author hex:</label>
-    <input type="text" class="form-control" value={authorHex} />
+    <input type="text" class="form-control" bind:value={authorHex} />
   </div>
   <div class="mb-3">
     <label for="" class="form-label">relays:</label>
-    <textarea class="form-control" value={relays} />
+    <textarea class="form-control" bind:value={relays} />
   </div>
   <div class="mt-2 text-center">
     <button class="btn btn-primary" on:click={getDatabase}>Get Table</button>
   </div>
   <div class="mt-4"><label for="" class="form-label">result</label></div>
-  <textarea class="form-control result" value={result} />
+  <textarea class="form-control result" bind:value={result} />
 </div>
 
 <style>
